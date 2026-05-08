22e381b96e5a66bbb3c14f7efcc976aafb77c9d7
G1OOO <gg.tandashvili@gmail.com>
2026-05-08 13:59:46 +0400

    changed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import BoardPage from "./pages/BoardPage";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import './index.css';
 
 const router = createBrowserRouter([
